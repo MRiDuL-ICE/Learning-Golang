75ef8c04c8b6a7c72e13d3e4d91f8b3c578d530c
MRiDuL-ICE <abdulwahab22400@gmail.com>
2025-03-24 16:32:17 +0600

    exploring more about slices in go

--- 08myslices/main.go
+++ 08myslices/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	var fruitList = []string{"Apple", "Tomato", "Peach"}
@@ -12,4 +15,19 @@ func main() {
 	fruitList = append(fruitList[2:3])
 
 	fmt.Println(fruitList)
+
+	highScores := make([]int, 4)
+
+	highScores[0] = 45
+	highScores[1] = 98
+	highScores[2] = 75
+	highScores[3] = 65
+
+	highScores = append(highScores, 35, 57, 49, 58)
+
+	sort.Ints(highScores)
+	fmt.Println(sort.IntsAreSorted(highScores))
+
+	fmt.Println(highScores)
+
 }
